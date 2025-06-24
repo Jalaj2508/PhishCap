f53a1fed68e449c665a74d82dee7a6cd4a783f08
Jalaj <jalaj2003@gmail.com>
2025-06-24 18:48:36 +0530

    PhishCap

--- app.py
+++ app.py
@@ -21,6 +21,13 @@ def index():
     if request.method == "POST":
 
         url = request.form["url"]
+
+        #Whitelist Websites
+        whitelist = ['google.com', 'microsoft.com', 'github.com']
+        if any(kw in url for kw in whitelist):
+            return render_template('index.html', xx=1.0, url=url)
+
+        #ML-based prediction
         obj = FeatureExtraction(url)
         x = np.array(obj.getFeaturesList()).reshape(1,30) 
 
@@ -30,10 +37,10 @@ def index():
         y_pro_phishing = gbc.predict_proba(x)[0,0]
         y_pro_non_phishing = gbc.predict_proba(x)[0,1]
         # if(y_pred ==1 ):
-        pred = "It is {0:.2f} % safe to go ".format(y_pro_phishing*100)
+        pred = "It is {0:.2f} % safe to go ".format(y_pro_non_phishing * 100)
         return render_template('index.html',xx =round(y_pro_non_phishing,2),url=url )
     return render_template("index.html", xx =-1)
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
