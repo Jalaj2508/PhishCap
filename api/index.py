119404bc16d10cdcf688ad66489398ca08345801
Jalaj <jalaj2003@gmail.com>
2025-07-17 11:44:43 +0530

    Updates index.py

--- api/index.py
+++ api/index.py
@@ -1,30 +1,47 @@
-import pickle
+#importing required libraries
+
+from flask import Flask, request, render_template
 import numpy as np
+import pandas as pd
+from sklearn import metrics 
+import warnings
+import pickle
+warnings.filterwarnings('ignore')
 from feature import FeatureExtraction
 
-def handler(request):
-    # Get URL from query or body
-    url = request.get("url")
-    if not url:
-        return {
-            "statusCode": 400,
-            "body": "Missing 'url' parameter"
-        }
+file = open("pickle/model.pkl","rb")
+gbc = pickle.load(file)
+file.close()
 
-    whitelist = ['google.com', 'microsoft.com', 'github.com']
-    if any(kw in url for kw in whitelist):
-        return {
-            "statusCode": 200,
-            "body": "1.0"
-        }
 
-    file = open("pickle/model.pkl","rb")
-    gbc = pickle.load(file)
-    file.close()
-    obj = FeatureExtraction(url)
-    x = np.array(obj.getFeaturesList()).reshape(1,30)
-    y_pro_non_phishing = gbc.predict_proba(x)[0,1]
-    return {
-        "statusCode": 200,
-        "body": str(round(y_pro_non_phishing,2))
-    }
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+
+        url = request.form["url"]
+
+        #Whitelist Websites
+        whitelist = ['google.com', 'microsoft.com', 'github.com']
+        if any(kw in url for kw in whitelist):
+            return render_template('index.html', xx=1.0, url=url)
+
+        #ML-based prediction
+        obj = FeatureExtraction(url)
+        x = np.array(obj.getFeaturesList()).reshape(1,30) 
+
+        y_pred =gbc.predict(x)[0]
+        #1 is safe       
+        #-1 is unsafe
+        y_pro_phishing = gbc.predict_proba(x)[0,0]
+        y_pro_non_phishing = gbc.predict_proba(x)[0,1]
+        # if(y_pred ==1 ):
+        pred = "It is {0:.2f} % safe to go ".format(y_pro_non_phishing * 100)
+        return render_template('index.html',xx =round(y_pro_non_phishing,2),url=url )
+    return render_template("index.html", xx =-1)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
+    
