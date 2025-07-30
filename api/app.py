3ba0d3694ebd993cb775a7b1be07bb7fa8b73831
Jalaj <jalaj2003@gmail.com>
2025-07-30 11:18:36 +0530

    Updates regarding Logo Visibility

--- api/app.py
+++ api/app.py
@@ -20,8 +20,11 @@ gbc = pickle.load(file)
 file.close()
 
 
-template_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'templates'))
-app = Flask(__name__, template_folder=template_dir)
+base_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+template_dir = os.path.join(base_dir, 'templates')
+static_dir = os.path.join(base_dir, 'static')
+
+app = Flask(__name__, template_folder=template_dir, static_folder=static_dir)
 
 @app.route("/", methods=["GET", "POST"])
 def index():
